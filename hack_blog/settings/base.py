e961bedd94e02976b4708d511eba33df8432ffa6
Niel <nielmce@gmail.com>
2016-07-25 16:24:00 +0100

    clean for merge

--- hack_blog/settings/base.py
+++ hack_blog/settings/base.py
@@ -102,7 +102,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)  # static directory at the project level
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # static directory at the project level
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
