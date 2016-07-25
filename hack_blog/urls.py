33258b68936575c6da2b505e0b98495ba6076dfc
Niel <nielmce@gmail.com>
2016-07-25 15:25:21 +0100

    media root file

--- hack_blog/urls.py
+++ hack_blog/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from blog import views
 from django.contrib.staticfiles import views as static_views
+import settings.base
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -26,5 +28,5 @@ urlpatterns = [
     url(r'^blog/(?P<id>\d+)/$', views.post_details, name="blogdetails"),
     url(r'^static/(?P<path>.*)$', static_views.serve),
     url(r'', include('accounts.urls')),
-
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.base.MEDIA_ROOT}),
 ]
